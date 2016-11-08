459f82417a6ca066134d5c5a68dc4b66742b74f6
mogh <mogh@example.com>
2016-11-07 23:12:40 -0800

    attempted to add task list

---
+++ client/backlog/backlog.js
@@ -0,0 +1,45 @@
+import { Template } from 'meteor/templating';
+ 
+import { Tasks } from './tasks.js';
+import './backlog.html';
+import './task.html';
+ 
+Template.body.helpers({
+  tasks: [
+    { text: 'This is task 1' },
+    { text: 'This is task 2' },
+    { text: 'This is task 3' },
+  ],
+});
+
+Template.body.events({
+  'submit .new-task'(event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+ 
+    // Get value from form element
+    const target = event.target;
+    const text = target.text.value;
+ 
+    // Insert a task into the collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), // current time
+    });
+ 
+    // Clear form
+    target.text.value = '';
+  },
+});
+ 
+Template.task.events({
+  'click .toggle-checked'() {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this._id, {
+      $set: { checked: ! this.checked },
+    });
+  },
+  'click .delete'() {
+    Tasks.remove(this._id);
+  },
+});
