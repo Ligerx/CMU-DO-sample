5de67be17a41ebb799a555db0d5ec35cfd34528e
awang1 <awang1@andrew.cmu.edu>
2016-11-12 19:49:16 -0500

    Fix shit

--- client/backlog/backlog.js
+++ client/backlog/backlog.js
@@ -1,7 +1,12 @@
 import { Template } from 'meteor/templating';
-import { Tasks } from '../api/tasks.js';
+import { Tasks } from '../../imports/api/tasks.js';
 
 import './backlog.html';
+
+Template.body.onCreated(function bodyOnCreated() {
+  // this.state = new ReactiveDict();
+  Meteor.subscribe('tasks');
+});
 
 Template.body.helpers({
   tasks() {
@@ -14,29 +19,33 @@ Template.body.events({
     console.log('attempting insert...');
     // Prevent default browser form submit
     event.preventDefault();
- 
+
     // Get value from form element
     const target = event.target;
     const text = target.text.value;
+
+    console.log('The Text is: ' + text);
+
     // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
- 
+    // Tasks.insert({
+    //   text,
+    //   createdAt: new Date(), // current time
+    // });
+
+
     // Clear form
     target.text.value = '';
   },
 });
- 
-Template.task.events({
-  'click .toggle-checked'() {
-    // Set the checked property to the opposite of its current value
-    Tasks.update(this._id, {
-      $set: { checked: ! this.checked },
-    });
-  },
-  'click .delete'() {
-    Tasks.remove(this._id);
-  },
-});
+
+// Template.task.events({
+//   'click .toggle-checked'() {
+//     // Set the checked property to the opposite of its current value
+//     Tasks.update(this._id, {
+//       $set: { checked: ! this.checked },
+//     });
+//   },
+//   'click .delete'() {
+//     Tasks.remove(this._id);
+//   },
+// });
