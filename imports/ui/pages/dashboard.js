92fbd2ae16ab78685da76f4f04f671067ec5103e
CatsFromMars <mduque@andrew.cmu.edu>
2016-11-21 01:14:18 -0500

    got started on fixing up task list

--- imports/ui/pages/dashboard.js
+++ imports/ui/pages/dashboard.js
@@ -2,7 +2,15 @@ import { Template } from 'meteor/templating';
 import { Tasks } from '../../api/api.js';
 
 import './dashboard.html';
+import '../components/newTask/newTask.js';
+import '../components/task/task.js';
 
-// Template.backlog.onCreated(function bodyOnCreated() {
-//   Meteor.subscribe('tasks');
-// });
+Template.dashboard.onCreated(function bodyOnCreated() {
+  Meteor.subscribe('tasks');
+});
+
+Template.dashboard.helpers({
+  tasks() {
+    return Tasks.find();
+  },
+});
