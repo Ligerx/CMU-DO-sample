e80a18b143f7e7cdc647fbfc973431eefd16c792
Suvrath Penmetcha <spenmetc@andrew.cmu.edu>
2016-11-29 22:14:03 -0500

    moved simple schema from methods to tasks.js

--- imports/api/tasks/tasks.js
+++ imports/api/tasks/tasks.js
@@ -5,6 +5,13 @@ export const Tasks = new Mongo.Collection('tasks');
 var Schemas = {};
 
 Schemas.Tasks = new SimpleSchema({
-	
-	
+  name: { type: String },
+  completed_on: {type: Date, optional: true},
+  created_on: {type: Date},
+  due_on: { type: Date },
+  is_urgent: { type: Boolean},
+  is_important: { type: Boolean}
+
 });
+
+Tasks.attachSchema(Schemas.Tasks);
