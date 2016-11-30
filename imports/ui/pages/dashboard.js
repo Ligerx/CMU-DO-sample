f01942b7a460e09080bbffb951154df8a3d95c10
awang1 <awang1@andrew.cmu.edu>
2016-11-30 18:08:18 -0500

    Create newTaskModal component and use that

--- imports/ui/pages/dashboard.js
+++ imports/ui/pages/dashboard.js
@@ -2,9 +2,8 @@ import { Template } from 'meteor/templating';
 import { Tasks } from '../../api/api.js';
 
 import './dashboard.html';
-import '../components/newTask/newTask.js';
 import '../components/taskList/taskList.js';
-import '../components/task/task.js';
+import '../components/newTaskModal/newTaskModal.js';
 
 Template.dashboard.onCreated(function bodyOnCreated() {
   Meteor.subscribe('tasks');
