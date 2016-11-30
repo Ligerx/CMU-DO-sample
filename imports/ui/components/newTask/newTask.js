a909f09a82c66ba4e369dcab8a2838f6bc3c41ca
awang1 <awang1@andrew.cmu.edu>
2016-11-30 13:43:43 -0500

    No longer clearing the form if the task failed to insert

--- imports/ui/components/newTask/newTask.js
+++ imports/ui/components/newTask/newTask.js
@@ -12,13 +12,13 @@ Template.datepicker.rendered = function() {
 
 Template.newTask.rendered = function() {
   console.log('attempting to render newTask component...');
-    $(".setPriority").popover({
-        html: true,
-        title: 'Select Priority',
-        content: function() {
-            return $("#popover-content").html();
-        }
-    });
+  $(".setPriority").popover({
+    html: true,
+    title: 'Select Priority',
+    content: function() {
+      return $("#popover-content").html();
+    }
+  });
 }
 
 Template.choosePriority.helpers({
@@ -37,32 +37,36 @@ Template.newTask.helpers({
 
 Template.newTask.events({
   'submit .new-task'(event) {
-    console.log('attempting insert...');
+    // console.log('attempting insert...');
+
     // Prevent default browser form submit
     event.preventDefault();
 
     // Get value from form element
     const target = event.target;
 
-    console.log(target);
-    // const category = target.selected;
-    // const dueDate = target.duedate.value;
+    // console.log(target);
     const name = target.name.value;
     const date = target.date.value; // not sure if I need to pass a string or a Date object?
 
-
-
-    console.log('The task is: ' + name);
-    console.log('The task due date is: ' + date);
+    // console.log('The task is: ' + name);
+    // console.log('The task due date is: ' + date);
 
     // Insert a task into the collection
-    Meteor.call('tasks.insert', name, date);
+    Meteor.call('tasks.insert', name, date, function(error) {
+      if(error) {
+        // Maybe some validation error here.
+        // e.g. say that a task name is required.
+      }
+      else {
+        // Clear form
+        target.name.value = '';
+        target.date.value = '';
+      }
+    });
 
     console.log('Num tasks is now: ' + Tasks.find().count());
 
-    // Clear form
-    target.name.value = '';
-    target.date.value = '';
   },
 
 });
