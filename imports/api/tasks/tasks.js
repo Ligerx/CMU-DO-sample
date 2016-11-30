8d14870d6c365bbad3e0e00b03a19797d4e15062
Suvrath Penmetcha <spenmetc@andrew.cmu.edu>
2016-11-29 23:10:15 -0500

    added default value

--- imports/api/tasks/tasks.js
+++ imports/api/tasks/tasks.js
@@ -4,11 +4,12 @@ var Schemas = {};
 
 Schemas.Tasks = new SimpleSchema({
   name: { type: String },
-  completed_on: {type: Date, optional: true},
-  created_on: {type: Date},
-  due_on: { type: Date },
-  is_urgent: { type: Boolean},
-  is_important: { type: Boolean}
+  completed_on: { type: Date, optional: true},
+  created_on: { type: Date},
+  due_on: { type: Date, optional: true},
+  is_sorted: { type: Boolean, defaultValue: false}
+  is_urgent: { type: Boolean, defaultValue: false},
+  is_important: { type: Boolean, defaultValue: false}
 
 });
 
