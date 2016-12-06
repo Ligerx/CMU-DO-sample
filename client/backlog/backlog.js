60485041f8d2f3fbdcabb07091ffb5ab4aaa32a4
mogh <mogh@example.com>
2016-12-05 18:48:24 -0800

    tasklist in response to PR

--- client/backlog/backlog.js
+++ client/backlog/backlog.js
@@ -1,18 +1,23 @@
 import { Template } from 'meteor/templating';
- 
-import { Tasks } from './tasks.js';
+import { Tasks } from '../api/tasks.js';
+
 import './backlog.html';
-import './task.html';
+
+Template.body.helpers({
+  tasks() {
+    return Tasks.find({});
+  },
+});
 
 Template.body.events({
   'submit .new-task'(event) {
+    console.log('attempting insert...');
     // Prevent default browser form submit
     event.preventDefault();
  
     // Get value from form element
     const target = event.target;
     const text = target.text.value;
- 	console.log('INTSERT!');
     // Insert a task into the collection
     Tasks.insert({
       text,
