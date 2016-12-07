fcf4d626b42311308a9f971fce53ea3eb8e1cdf5
awang1 <awang1@andrew.cmu.edu>
2016-12-06 21:03:02 -0500

    I think I got the newTaskForm to work how I want

--- imports/ui/components/newTaskForm/newTaskForm.js
+++ imports/ui/components/newTaskForm/newTaskForm.js
@@ -1,4 +1,27 @@
 import { Template } from 'meteor/templating';
+import { Tasks } from '../../../api/api.js';
 
 import './newTaskForm.html';
 import '../taskForm/taskForm.js';
+
+Template.newTaskForm.helpers({
+  handleSubmit() {
+    return function(name, date, is_sorted, is_urgent, is_important) {
+      console.log('newTaskForm: handling submit!');
+
+      Meteor.call('tasks.insert', name, date, is_sorted, is_urgent, is_important, function(error) {
+        if(error) {
+          // Maybe some validation error here.
+          // e.g. say that a task name is required.
+        }
+        else {
+          // Clear form
+          target.name.value = '';
+          target.date.value = '';
+        }
+      });
+
+      console.log('Num tasks is now: ' + Tasks.find().count());
+    }
+  }
+});
--- imports/ui/components/taskForm/taskForm.js
+++ imports/ui/components/taskForm/taskForm.js
@@ -11,17 +11,6 @@ Template.datepicker.rendered = function() {
   });
 };
 
-// Template.newTask.rendered = function() {
-//   console.log('attempting to render newTask component...');
-//   $(".setPriority").popover({
-//     html: true,
-//     title: 'Select Priority',
-//     content: function() {
-//       return $("#popover-content").html();
-//     }
-//   });
-// }
-
 Template.choosePriority.helpers({
   taskCount: function() {
     //Change later
@@ -35,35 +24,20 @@ Template.taskForm.onCreated(function() {
 
 Template.taskForm.helpers({
   printPriority() {
-  //// This is broken, similar method in task.js you might want to refer to
-  //   const task = this.task;
-  //   const array = [];
-
-  //   const priorities = {
-  //     'is_important': 'Important',
-  //     'is_urgent': 'Urgent',
-  //   }
-
-  //   priorities.forEach(function({val, print}) {
-  //     if( task[val] ) { array.push(print); }
-  //   });
-
-  //   return array.join(' and ');
-  // },
     return Template.instance().currentPriority.get();
   }
 });
 
 Template.taskForm.events({
-  'click #submit-close':function(){
+  'click #submit-close'(){
   $('#newTaskModal').modal('hide');
   },
 
-  'click #select-priority':function(){
+  'click #select-priority'(){
     $(".priority-select").css('visibility', 'visible');
   },
 
-  'change .prioritySelect': function(event, template){
+  'change .prioritySelect'(event, template){
     console.log(event.currentTarget.value);
     switch(event.currentTarget.value) {
       case '1':
@@ -82,60 +56,38 @@ Template.taskForm.events({
   },
 
   'submit .new-task'(event) {
-    // console.log('attempting insert...');
-
     // Prevent default browser form submit
     event.preventDefault();
 
     // Get value from form element
     const target = event.target;
 
-    // console.log(target);
     var is_urgent = false;
     var is_important = false;
     var is_backlog = false;
     const name = target.name.value;
-    const date = target.date.value; // not sure if I need to pass a string or a Date object?
+    const date = target.date.value;
     const radioValue = event.target.priority.value;
 
     console.log('The radio value is: ' + radioValue);
     if(radioValue == "1"){
-      //Important-Urgent
       is_important = true;
       is_urgent = true;
     }
     if(radioValue == "2") {
-      //Important
       is_important = true;
     }
     if(radioValue == "3") {
-      //Urgent
       is_urgent = true;
     }
     if(radioValue == "4") {
-      //Urgent
       is_backlog = true;
     }
 
-    // console.log('The task is: ' + name);
-    // console.log('The task due date is: ' + date);
+    var is_sorted = is_urgent || is_important || is_backlog;
 
-    // Insert a task into the collection
-    var s = is_urgent||is_important||is_backlog;
-    Meteor.call('tasks.insert', name, date, s, is_urgent, is_important, function(error) {
-      if(error) {
-        // Maybe some validation error here.
-        // e.g. say that a task name is required.
-      }
-      else {
-        // Clear form
-        target.name.value = '';
-        target.date.value = '';
-      }
-    });
-
-    console.log('Num tasks is now: ' + Tasks.find().count());
-
+    // Trigger the callback to the parent template
+    this.onSubmit(name, date, is_sorted, is_urgent, is_important);
   },
 
 });
