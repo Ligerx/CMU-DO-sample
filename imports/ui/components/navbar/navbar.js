5ec8d6419d64971ca2489e786bf454e19e318cc6
Suvrath Penmetcha <spenmetc@andrew.cmu.edu>
2016-12-15 19:48:31 -0500

    added login, logout etc

--- imports/ui/components/navbar/navbar.js
+++ imports/ui/components/navbar/navbar.js
@@ -2,3 +2,18 @@ import { Template } from 'meteor/templating';
 // import { Tasks } from '../../../api/api.js';
 
 import './navbar.html';
+
+Template.navbar.events({
+  'click .logout': function(event){
+      event.preventDefault();
+      Meteor.logout();
+  }
+});
+
+
+Template.navbar.helpers({
+	email(){
+		console.log(Meteor.user());
+		return Meteor.user().email;
+	}
+});
